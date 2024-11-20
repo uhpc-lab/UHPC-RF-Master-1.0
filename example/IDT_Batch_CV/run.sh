2b41cf271d52a788cf0547099fa165fded01ce5a
asslua <adrianshalom.lua@gmail.com>
2024-11-20 23:32:51 +0800

    Add files via upload

---
+++ example/IDT_Batch_CV/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -N IDT_calculation
+#PBS -l nodes=1:ppn=4
+#PBS -l walltime=00:30:00
+#PBS -l mem=4g
+#PBS -q h07c02
+
+
+EXEC=/puhome/21093985d/UHPC_RF_Master_1_copy/app/ConstantVolume/IDT_calculation_CV
+INPUT=/puhome/21093985d/UHPC_RF_Master_1_copy/example/IDT_Batch_CV/input.txt
+mkdir -p -- "results"
+OUTFILE=/puhome/21093985d/UHPC_RF_Master_1_copy/example/IDT_Batch_CV/results/result.out
+
+# Run your executable with the input file
+
+stdbuf -oL $EXEC $INPUT 2>&1 | tee $OUTFILE
+
+
